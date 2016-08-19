404b025137b230ad56d53a54ffd778578d465942
Ugo Rosini <rosini.u@gmail.com>
2016-08-20 01:28:16 +0200

    Check floor

--- MeteoJam/Assets/Scripts/MovePlayer.cs
+++ MeteoJam/Assets/Scripts/MovePlayer.cs
@@ -6,15 +6,22 @@ public class MovePlayer : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private Vector2 movement;
     private bool grounded = false;
+    private int platformLayer;
 
-	// Use this for initialization
-	void Start ()
+    void OnGUI()
+    {
+        //GUILayout.Label("grounded : " + grounded);
+    }
+
+    // Use this for initialization
+    void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        platformLayer = 1 << LayerMask.NameToLayer("Platform");
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         movement = Vector2.zero;
 
@@ -23,26 +30,36 @@ public class MovePlayer : MonoBehaviour
         else if (Input.GetAxis("Horizontal") < 0)
             movement += Vector2.left;
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && grounded)
             Jump();
+
+        DetectFloor();
     }
 
     void FixedUpdate()
     {
-        //_rigidbody.velocity = Vector2.zero;
         Move(movement);
     }
 
     void Move(Vector2 move)
     {
-        //_rigidbody.velocity += movement * GameManager.instance.playerSpeed * Time.deltaTime;
-        //_rigidbody.MovePosition(_rigidbody.position + move);
-        _rigidbody.position += move * GameManager.instance.playerSpeed * Time.deltaTime; 
+        _rigidbody.position += move * GameManager.instance.playerSpeed * Time.deltaTime;
     }
 
     void Jump()
     {
-        _rigidbody.AddForce(Vector2.up*GameManager.instance.jumpForce, ForceMode2D.Impulse);
+        _rigidbody.AddForce(Vector2.up * GameManager.instance.jumpForce, ForceMode2D.Impulse);
+    }
+
+    void DetectFloor()
+    {
+        grounded = false;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up , 0.5f, platformLayer);
+        Debug.DrawLine(transform.position, transform.position - Vector3.up * 0.5f,Color.red);
+        if (hit.collider != null)
+        {
+            grounded = true;
+        }
     }
 }
-
